acfeae24006a52d8c1f5a4192b7c61c13a966a95
nclqq <48111332+nclqq@users.noreply.github.com>
2019-04-07 20:49:18 +0800

    Update test_user_loadUserList.py

--- testcase/user/test_user_loadUserList.py
+++ testcase/user/test_user_loadUserList.py
@@ -1,26 +1,26 @@
-import allure
-from conftest import http,common
+#import allure
+#from conftest import http,common
 
 
-@allure.feature("user下的加载用户列表")
-class Test_LoadUserList:
+#@allure.feature("user下的加载用户列表")
+#class Test_LoadUserList:
 
 
     # 测试用例体
-    @allure.story("加载用户列表")
-    def test_loadUserList(self):
-        userList_path='/user/loadUserList'
-        userList_data = {
-            'pageSize': 30,
-            'pageCurrent':1
-        }
+  #  @allure.story("加载用户列表")
+  #  def test_loadUserList(self):
+  #      userList_path='/user/loadUserList'
+  #      userList_data = {
+  #          'pageSize': 30,
+  #          'pageCurrent':1
+  #      }
         # 调用httpUtil的post方法
-        resp_login = http.post(userList_path, userList_data)
+   #     resp_login = http.post(userList_path, userList_data)
 
-        print(resp_login['object']['list'][0]['id'])
-        print(common.newUserId)
+   #     print(resp_login['object']['list'][0]['id'])
+   #     print(common.newUserId)
 
-        assert resp_login['code']==200
-        assert resp_login['msg'] == '操作成功'
-        assert resp_login['object']['list'][0]['id'] == common.newUserId
-        print("pass------------\n")
+    #    assert resp_login['code']==200
+    #    assert resp_login['msg'] == '操作成功'
+     #   assert resp_login['object']['list'][0]['id'] == common.newUserId
+     #   print("pass------------\n")
