16ad208761a2dcf4479cb929480f8a0f295b30c5
nclqq <48111332+nclqq@users.noreply.github.com>
2019-04-07 20:50:49 +0800

    Update test_user_login.py

--- testcase/user/test_user_login.py
+++ testcase/user/test_user_login.py
@@ -1,30 +1,30 @@
-import allure
-from conftest import http,common
+#import allure
+#from conftest import http,common
 
 
 
-@allure.feature("user下的登录模块")
-class Test_Login:
+#@allure.feature("user下的登录模块")
+#class Test_Login:
 
     # 测试用例体
-    @allure.story("登录成功")
-    def test_userLogin(self):
-        login_path='/sys/login'
-        login_data = {
-            'userName':common.newUserName,
-            'password':common.admin_password
-        }
+#    @allure.story("登录成功")
+#    def test_userLogin(self):
+#        login_path='/sys/login'
+#       login_data = {
+ #           'userName':common.newUserName,
+ #           'password':common.admin_password
+ #       }
         # 调用httpUtil的post方法
-        resp_login = http.post(login_path, login_data)
+  #      resp_login = http.post(login_path, login_data)
 
-        assert resp_login['code']==200
-        assert resp_login['msg'] == '操作成功'
+ #       assert resp_login['code']==200
+ #       assert resp_login['msg'] == '操作成功'
         # assert resp_login['object']['userId'] == 263
         #将新注册的id传到公共数据中
-        common.newUserId=resp_login['object']['userId']
+  #      common.newUserId=resp_login['object']['userId']
 
-        print(resp_login['object']['userId'])
-        print("pass------------\n")
-        return resp_login['object']['userId']
+  #      print(resp_login['object']['userId'])
+  #      print("pass------------\n")
+  #      return resp_login['object']['userId']
 
 
